8b88550c8c425a042cb64ead1a395a248ba5d551
Emc54 <35582806+Emc54@users.noreply.github.com>
2024-01-06 16:06:29 +0000

    2015 day 6 sol1

--- 2015/Day06/sol1.py
+++ 2015/Day06/sol1.py
@@ -1,5 +1,33 @@
 import numpy as np
+import re
 
-lights = np.zeros(1000,1000)
+lights  = np.zeros((1000,1000), dtype=bool)
 
+with open("input","r") as f:
+    data = f.read().split("\n")
+
+for datapoint in data:
+
+    splits = re.split("([0-9])", datapoint, maxsplit=1)
+    command = splits[0]
+    values = "".join([splits[1],splits[2]])
+    part1, _, part2 = re.split(" ", values)
+    x1, y1 = re.split(",", part1)
+    x2, y2 = re.split(",", part2)
+        
+    x1 = int(x1)
+    x2 = int(x2) + 1
+    y1 = int(y1)
+    y2 = int(y2) + 1
+
+    if command == "toggle ":
+        lights[x1:x2, y1:y2] = ~lights[x1:x2, y1:y2] 
+        
+    if command == "turn off ":
+        lights[x1:x2, y1:y2] = 0
+
+    if command == "turn on ":
+        lights[x1:x2, y1:y2] = 1
+    
+print(np.sum(lights))
 print(lights)
